078b301b900ba632281dee7f807a473178a0f09d
Joshua Daniel <joshua_dup@hotmail.com>
2023-03-05 23:00:27 -0500

    closures

--- src/scope/scope.js
+++ src/scope/scope.js
@@ -1,3 +1,9 @@
+/* Function Scope */
+/* 
+Todas y cada una de las funciones que hagamos dentro de la logica del programa van a tener su propio alcance y las variables
+declaradas dentro de esta funcion van a ser accesibles solamente dentro de esta funcion y cualquiera de sus funciones anidadas.
+*/
+
 function greeting() {
   let userName = "Tanjiro";
   console.log(userName);
@@ -8,9 +14,6 @@ function greeting() {
 }
 
 greeting();
+
+// si quiero acceder a esta variable no sera posible porque no esta asignada como global. El alcance sera solo dentro de esa funcion
 console.log(userName);
-/* Function Scope */
-/* 
-Todas y cada una de las funciones que hagamos dentro de la logica del programa van a tener su propio alcance y las variables
-declaradas dentro de esta funcion van a ser accesibles solamente dentro de esta funcion y cualquiera de sus funciones anidadas.
-*/
